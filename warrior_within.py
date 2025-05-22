8db4cabc19685bdcbf23e8bee6ee50d71beb0848
YaroslavVdovin <87266278+YaroslavVdovin@users.noreply.github.com>
2025-05-22 14:59:38 +0300

    Update warrior_within.py

--- warrior_within.py
+++ warrior_within.py
@@ -1,22 +1,23 @@
+import random
+
 class Warmonger:
-    def __init__(self, name, enemy='', healthbar=100):
+    def __init__(self, name, healthbar=100):
         self.healthbar = healthbar
         self.name = name
-        self.enemy = enemy
+
     def protect(self):
         self.healthbar -= 20
-        if self.healthbar > 0:
-            print("%s успешно атаковал %s. У %s осталось %s здоровья" % (self.enemy, self.name, self.name, self.healthbar))
-        else:
-            print("%s откис %s празднует победу." % (self.name, self.enemy))
 
 if __name__ == "__main__":
     first_player = Warmonger("Leroy Jenkins")
     second_player = Warmonger("Nerzul")
-    first_player.enemy = second_player.name
-    second_player.enemy = first_player.name
     battle_list = [first_player, second_player]
 
     while first_player.healthbar > 0 and second_player.healthbar > 0:
         defender = random.choice(battle_list)
+        attacker = [i for i in battle_list if i != defender][0]
         defender.protect()
+        if defender.healthbar > 0:
+            print("%s успешно атаковал %s. У %s осталось %s здоровья" % (attacker.name, defender.name, defender.name, defender.healthbar))
+        else:
+            print("%s откис %s празднует победу." % (defender.name, attacker.name))
