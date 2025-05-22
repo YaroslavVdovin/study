90afa437ab311f664f8f07211cce07a6489345fe
YaroslavVdovin <87266278+YaroslavVdovin@users.noreply.github.com>
2025-05-22 12:00:47 +0300

    Update warrior_within.py

--- warrior_within.py
+++ warrior_within.py
@@ -1,23 +1,22 @@
-import random
-
 class Warmonger:
-    def __init__(self, name, healthbar=100):
+    def __init__(self, name, enemy='', healthbar=100):
         self.healthbar = healthbar
         self.name = name
-    def attack(self, enemy):
-        enemy.healthbar -= 20
-        if enemy.healthbar > 0:
-            print("%s успешно атаковал %s. У %s осталось %s здоровья" % (self.name, enemy.name, enemy.name, enemy.healthbar))
+        self.enemy = enemy
+    def protect(self):
+        self.healthbar -= 20
+        if self.healthbar > 0:
+            print("%s успешно атаковал %s. У %s осталось %s здоровья" % (self.enemy, self.name, self.name, self.healthbar))
         else:
-            print("%s откис %s празднует победу." % (enemy.name, self.name))
+            print("%s откис %s празднует победу." % (self.name, self.enemy))
 
 if __name__ == "__main__":
     first_player = Warmonger("Leroy Jenkins")
     second_player = Warmonger("Nerzul")
-
+    first_player.enemy = second_player.name
+    second_player.enemy = first_player.name
     battle_list = [first_player, second_player]
 
     while first_player.healthbar > 0 and second_player.healthbar > 0:
-        attacker = random.choice(battle_list)
-        defender = random.choice([i for i in battle_list if i is not attacker])
-        attacker.__attack(defender)
+        defender = random.choice(battle_list)
+        defender.protect()
