757dd051a181087089369d96a29b1cbe2d49d8dc
YaroslavVdovin <87266278+YaroslavVdovin@users.noreply.github.com>
2025-05-24 21:28:34 +0300

    Update django.py

--- django.py
+++ django.py
@@ -5,13 +5,13 @@ from warrior_within import Warmonger
 class Model:
     @classmethod
     def __get_attributes(cls):
-        return [i for i in dir(cls) if i[1] != '_']  # генератор списков + все атрибуты, которые не начинаются с _
+        return [i for i in dir(cls) if i[0] != '_']  # генератор списков + все атрибуты
     @classmethod
     def save(cls):
         """Функция сохранения атрибутов класса в JSON"""
         class_name = cls.__name__
         attr_list = cls._Model__get_attributes()
-        with open("%s_attr_list.txt" % class_name, "w") as f_class_attr: # создаем файл/перезаписываем всё содержание файла с шаблонным названием
+        with open(f"{class_name}_attr_list.txt", "w") as f_class_attr: # создаем файл/перезаписываем всё содержание файла с шаблонным названием
             f_class_attr.write(json.dumps(attr_list))
 
 class WarmongerUPD(Warmonger, Model):
