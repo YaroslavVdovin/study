923f1a1c879879aced778bc2008bd969013e46e4
YaroslavVdovin <87266278+YaroslavVdovin@users.noreply.github.com>
2025-08-14 16:39:23 +0300

    Update urls.py

--- django_course/test_project/first_app/urls.py
+++ django_course/test_project/first_app/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from .views import index, home, creed, navigation_task, blog_middle, hard_blog
 
 urlpatterns = [
-    path('index/', index),
     path('home/', home, name='home'),
     path('creed/', creed, name='creed'),
     path('navigating/', navigation_task, name='navigation_task'),
     path('blog_middle/', blog_middle, name='blog_middle'),
     path('hard/', hard_blog, name='hard_blog')
+
 ]
