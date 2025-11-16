7603f8cea6cef229438b42c35087744af86b123c
YaroslavVdovin <87266278+YaroslavVdovin@users.noreply.github.com>
2025-11-16 17:48:41 +0300

    Delete Module15/testrtes.py

--- Module15/testrtes.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect("database_test.sqlite")
-cursor = con.cursor()
-
-cursor.execute("SELECT student_id, course_id FROM StudentCourses;")
-print(cursor.fetchall())
